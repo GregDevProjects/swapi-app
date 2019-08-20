1a2a0e4a15855350aa5445226533234ed56a6e7f
GregDevProjects <gdonaldsonmclean@gmail.com>
2019-08-19 22:36:49 -0300

    feat(react): select star wars character with buttons and text input
    remove default template

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,69 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App () {
+  const [starWarsCharacter, setStarWarsCharacter] = useState(false)
+  const getStarWarsCharacter = (id) => {
+    fetch(`${process.env.REACT_APP_BASE_URL}star-wars-characters/${id}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setStarWarsCharacter(result)
+        },
+        (error) => {
+          console.warn(error)
+        }
+      )
+  }
+  const textInput = React.createRef()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => { getStarWarsCharacter(1) }}>Luke</button>
+      <button onClick={() => { getStarWarsCharacter(2) }}>C-3PO</button>
+      <button onClick={() => { getStarWarsCharacter(3) }}>R2-D2</button>
+      <button onClick={() => { getStarWarsCharacter(4) }}>Darth Vader</button>
+      <button onClick={() => { getStarWarsCharacter(5) }}>Leia Organa</button>
+      <input ref={textInput}></input><button onClick={() => { getStarWarsCharacter(textInput.current.value) }}>Search</button>
+      {starWarsCharacter && (
+      <>
+        <div>{starWarsCharacter.name}</div>
+        <div>{starWarsCharacter.height}</div>
+        <div>{starWarsCharacter.mass}</div>
+        <div>{starWarsCharacter.hairColor}</div>
+        <div>{starWarsCharacter.skinColor}</div>
+        <div>{starWarsCharacter.gender}</div>
+        <div>{starWarsCharacter.birthYear}</div>
+        <h3>homePlanet</h3>
+        <div>{starWarsCharacter.homePlanet.title}</div>
+        <div>{starWarsCharacter.homePlanet.terrain}</div>
+        <div>{starWarsCharacter.homePlanet.population}</div>
+        <h3>species</h3>
+        {starWarsCharacter.species.map((species, i) => {
+          // Return the element. Also pass key
+          return (
+            <div key={i}>
+              <div >{species.name}</div>
+              <div >{species.averageLifespan}</div>
+              <div >{species.classification}</div>
+              <div >{species.language}</div>
+            </div>
+          )
+        })}
+        <h3>films</h3>
+        {starWarsCharacter.films.map((film, i) => {
+          // Return the element. Also pass key
+          return (
+            <div key={i}>
+              <div >{film.title}</div>
+              <div >{film.director}</div>
+              <div >{film.producers}</div>
+              <div >{film.date}</div>
+            </div>
+          )
+        })}
+      </>
+      )}
+
     </div>
   )
 }
